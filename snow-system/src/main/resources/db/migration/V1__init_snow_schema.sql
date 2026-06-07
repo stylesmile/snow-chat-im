e6209d47eb65250f3828f08c5ee008102f4b98e0
Stylesmile <3239866994@qq.com>
2026-06-07 22:52:23 +0800

    集成flyway

--- snow-system/src/main/resources/db/migration/V1__init_snow_schema.sql
+++ snow-system/src/main/resources/db/migration/V1__init_snow_schema.sql
@@ -1,18 +1,5 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : localhost_3306
- Source Server Type    : MySQL
- Source Server Version : 80020
- Source Host           : localhost:3306
- Source Schema         : snow
-
- Target Server Type    : MySQL
- Target Server Version : 80020
- File Encoding         : 65001
-
- Date: 22/05/2020 16:36:57
-*/
+CREATE DATABASE IF NOT EXISTS `snow` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `snow`;
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
