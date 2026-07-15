74321f9651e136ed6d33d15e2243cc943eab91f6
mmm <mm@m.m>
2026-07-15 16:15:42 -0700

    MqttTopics

--- snow-chat-im-backend/snow-im-api/src/main/java/com/stylesmile/chat/mqtt/MqttTopics.java
+++ snow-chat-im-backend/snow-im-api/src/main/java/com/stylesmile/chat/mqtt/MqttTopics.java
@@ -5,10 +5,20 @@ public final class MqttTopics {
     private MqttTopics() {
     }
 
+    /**
+     * 获取用户 topic
+     * @param userId 用户id
+     * @return String
+     */
     public static String user(Integer userId) {
         return "chat/user/" + userId;
     }
 
+    /**
+     * 获取群topic
+     * @param groupId 群id
+     * @return String
+     */
     public static String group(Integer groupId) {
         return "chat/group/" + groupId;
     }
