17ee91e6bb8a596c197b5c5b2a73b24abb0cd8a7
mmm <mm@m.m>
2026-09-15 21:39:05 +0800

    启动报错

--- snow-chat-flutter/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ snow-chat-flutter/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -91,6 +91,11 @@ public final class GeneratedPluginRegistrant {
       Log.e(TAG, "Error registering plugin sqlite3_flutter_libs, eu.simonbinder.sqlite3_flutter_libs.Sqlite3FlutterLibsPlugin", e);
     }
     try {
+      flutterEngine.getPlugins().add(new io.flutter.plugins.urllauncher.UrlLauncherPlugin());
+    } catch (Exception e) {
+      Log.e(TAG, "Error registering plugin url_launcher_android, io.flutter.plugins.urllauncher.UrlLauncherPlugin", e);
+    }
+    try {
       flutterEngine.getPlugins().add(new io.flutter.plugins.videoplayer.VideoPlayerPlugin());
     } catch (Exception e) {
       Log.e(TAG, "Error registering plugin video_player_android, io.flutter.plugins.videoplayer.VideoPlayerPlugin", e);
