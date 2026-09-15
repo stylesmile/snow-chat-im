feb0dc048bddfe26622ac7d51f88c4bf174a2f64
mmm <mm@m.m>
2026-09-15 21:39:17 +0800

    启动报错GeneratedPluginRegistrant.swift

--- snow-chat-flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ snow-chat-flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -18,6 +18,7 @@ import record_macos
 import shared_preferences_foundation
 import sqflite_darwin
 import sqlite3_flutter_libs
+import url_launcher_macos
 import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -34,5 +35,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
